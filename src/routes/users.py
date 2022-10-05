40e945480f01c4102028502d0c37bd80d313ebc0
ushieru <uzielcocolan@gmail.com>
2022-10-05 14:25:00 -0500

    Add user controller

---
+++ src/routes/users.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+from src.controllers.users_controller import get_users
+from src.decorators.require_bearer_token import require_bearer_token
+
+users = Blueprint('users', __name__, url_prefix='/users')
+
+
+@users.route('/', methods=['GET'])
+@require_bearer_token
+def index(user):
+    return {'users': [user.toJson() for user in get_users(user)]}
