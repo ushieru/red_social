e2aed630dded23e439263d1931ce74c599de988f
ushieru <uzielcocolan@gmail.com>
2022-10-05 18:21:08 -0500

    Add seed

---
+++ scripts/seed.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.10
+import sys
+import os
+sys.path.append(os.getcwd())
+from src.controllers.users_controller import add_friend_request
+from src.controllers.auth_controller import create_user
+
+if __name__ == '__main__':
+    user1 = create_user('Feidlimid Baako Ellington',
+                        'feidlimid@email.com', 'feidlimid')
+    user2 = create_user('Camryn Bala Stacey', 'camryn@email.com', 'camryn')
+    user3 = create_user('Praise Onyekachi Heike', 'praise@email.com', 'praise')
+
+    add_friend_request(user1, user2.id)
+    add_friend_request(user2, user1.id)
+
+    add_friend_request(user1, user3.id)
