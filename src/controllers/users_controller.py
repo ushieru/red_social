3cba8861ae618731fdcc95d323b909a783130c4f
ushieru <uzielcocolan@gmail.com>
2022-10-05 18:36:33 -0500

    Delete friend logic

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -1,43 +1,9 @@
-import uuid
 from typing import List
-from sqlalchemy import select, and_
+from sqlalchemy import select
 from src.database.database import database
-from src.models import User, FriendRequest, Friend
+from src.models import User
 
 
 def get_users(user: User) -> List[User]:
     statement = select(User).where(User.id.is_not(user.id))
     return database.execute(statement).scalars().all()
-
-
-def get_friend_requests(user: User) -> List[FriendRequest]:
-    statement = select(FriendRequest).where(
-        FriendRequest.user_target_id.is_(user.id))
-    return database.execute(statement).scalars().all()
-
-
-def add_friend_request(user: User, user_id: str) -> FriendRequest:
-    statement = select(FriendRequest).where(
-        and_(FriendRequest.user_target_id.is_(user.id), FriendRequest.user_source_id.is_(user_id)))
-    result = database.execute(statement).fetchone()
-    if result:
-        _add_friend(user, user_id)
-        return result[0]
-    friend_request = FriendRequest(
-        id=str(uuid.uuid4()), user_source_id=user.id, user_target_id=user_id)
-    try:
-        database.add(friend_request)
-        database.commit()
-    except:
-        database.rollback()
-    return friend_request
-
-
-def _add_friend(user: User, user_id: str) -> Friend:
-    try:
-        friend = Friend(
-            id=str(uuid.uuid4()), user_source_id=user.id, user_target_id=user_id)
-        database.add(friend)
-        database.commit()
-    except:
-        database.rollback()
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from src.controllers.users_controller import get_users, add_friend_request
+from src.controllers.users_controller import get_users
 from src.decorators.require_bearer_token import require_bearer_token
 
 users = Blueprint('users', __name__, url_prefix='/users')
@@ -9,10 +9,3 @@ users = Blueprint('users', __name__, url_prefix='/users')
 @require_bearer_token
 def index(user):
     return [user.toJson() for user in get_users(user)]
-
-
-@users.route('/add_friend/<string:user_id>', methods=['POST'])
-@require_bearer_token
-def add_friend(user, user_id):
-    friend_request = add_friend_request(user, user_id)
-    return friend_request.toJson()
