f138fc8f5fe6bce85f3d348940feecece11ada8a
ushieru <uzielcocolan@gmail.com>
2022-10-05 13:54:53 -0500

    Add dont env

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
-from .routes.auth import auth
+from src.routes.auth import auth
+from src.decorators.require_bearer_token import require_bearer_token
+
+load_dotenv()
 
 app = Flask('Red Social')
+
+app.config['JWT_SECRET'] = os.getenv('JWT_SECRET')
 
 
 @app.route('/')
