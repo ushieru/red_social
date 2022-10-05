97c108f67a6782dde5bae2895e75e55a71082de4
ushieru <uzielcocolan@gmail.com>
2022-10-05 18:21:03 -0500

    Fix imports

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,4 +1,9 @@
-from src.models import User, FriendRequest
+#!/usr/bin/env python3.10
+import sys
+import os
+sys.path.append(os.getcwd())
 from src.database import engine, Base
+from src.models import User, FriendRequest
 
-Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
