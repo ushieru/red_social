0a337e9c7406d92bb52c6db4956decf2de1f9386
ushieru <uzielcocolan@gmail.com>
2022-10-05 16:53:36 -0500

    Fix users route

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from src.controllers.users_controller import get_users
+from src.controllers.users_controller import get_users, add_friend
 from src.decorators.require_bearer_token import require_bearer_token
 
 users = Blueprint('users', __name__, url_prefix='/users')
@@ -8,4 +8,11 @@ users = Blueprint('users', __name__, url_prefix='/users')
 @users.route('/', methods=['GET'])
 @require_bearer_token
 def index(user):
-    return {'users': [user.toJson() for user in get_users(user)]}
+    return [user.toJson() for user in get_users(user)]
+
+
+@users.route('/add_friend/<string:user_id>', methods=['POST'])
+@require_bearer_token
+def add_friend_request(user, user_id):
+    friend_request = add_friend(user, user_id)
+    return friend_request.toJson()
