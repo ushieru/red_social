26291e074705c2fd8a0f57e2e8992fd4601b7d69
ushieru <uzielcocolan@gmail.com>
2022-10-05 14:03:51 -0500

    Delete unused import

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from src.routes.auth import auth
-from src.decorators.require_bearer_token import require_bearer_token
 
 load_dotenv()
 
