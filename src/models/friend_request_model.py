6ebf96d7e2b3ba82c9c4d746cb0811e1cd133a8e
ushieru <uzielcocolan@gmail.com>
2022-10-07 17:22:57 -0500

    Fix add relations

--- src/models/friend_request_model.py
+++ src/models/friend_request_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, UniqueConstraint
+from sqlalchemy import Column, String, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship
 from src.database import Base
 
 
@@ -6,8 +7,11 @@ class FriendRequest(Base):
     __tablename__ = "friend_request"
 
     id = Column(String, primary_key=True, nullable=False)
-    user_source_id = Column(String, nullable=False)
-    user_target_id = Column(String, nullable=False)
+    user_source_id = Column(String, ForeignKey("user.id"), nullable=False)
+    user_target_id = Column(String, ForeignKey("user.id"), nullable=False)
+
+    user_source = relationship("User", foreign_keys=[user_source_id])
+    user_target = relationship("User", foreign_keys=[user_target_id])
 
     __table_args__ = (
         UniqueConstraint('user_source_id', 'user_target_id',
@@ -20,6 +24,6 @@ class FriendRequest(Base):
     def toJson(self):
         return {
             'id': self.id,
-            'user_source_id': self.user_source_id,
-            'user_target_id': self.user_target_id
+            'user_source': self.user_source.toJson(),
+            'user_target': self.user_target.toJson()
         }
