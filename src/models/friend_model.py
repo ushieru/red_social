835561dcf6eaa05acd92ce83bfc8bbbf8f26c3ef
ushieru <uzielcocolan@gmail.com>
2022-10-07 17:20:19 -0500

    Fix add relations

--- src/models/friend_model.py
+++ src/models/friend_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, UniqueConstraint
+from sqlalchemy import Column, String, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship
 from src.database import Base
 
 
@@ -6,8 +7,11 @@ class Friend(Base):
     __tablename__ = "friend"
 
     id = Column(String, primary_key=True, nullable=False)
-    user_source_id = Column(String, nullable=False)
-    user_target_id = Column(String, nullable=False)
+    user_source_id = Column(String, ForeignKey("user.id"), nullable=False)
+    user_target_id = Column(String, ForeignKey("user.id"), nullable=False)
+
+    user_source = relationship("User", foreign_keys=[user_source_id])
+    user_target = relationship("User", foreign_keys=[user_target_id])
 
     __table_args__ = (
         UniqueConstraint('user_source_id', 'user_target_id',
@@ -17,11 +21,11 @@ class Friend(Base):
     )
 
     def __repr__(self):
-        return f"Friend(id={self.id!r}, user_source_id={self.user_source_id!r}, user_target_id={self.user_target_id!r})"
+        return f"Friend(id={self.id!r}, user_source={self.user_source!r}, user_target={self.user_target!r})"
 
     def toJson(self):
         return {
             'id': self.id,
-            'user_source_id': self.user_source_id,
-            'user_target_id': self.user_target_id
+            'user_source': self.user_source.toJson(),
+            'user_target': self.user_target.toJson()
         }
