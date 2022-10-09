5023b5edf8416839596fcdf8ffbf6e8300489e92
ushieru <uzielcocolan@gmail.com>
2022-10-08 19:27:49 -0500

    Fix create user is move to -> users controller

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,17 +1,7 @@
-import uuid
 import hashlib
 from sqlalchemy import select, and_
 from src.database.database import database
 from src.models.user_model import User
-
-
-def create_user(name: str, email: str, password: str) -> User:
-    hash_pass = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    user = User(id=str(uuid.uuid4()), name=name,
-                email=email, password=hash_pass)
-    database.add(user)
-    database.commit()
-    return user
 
 
 def get_user_auth(email: str, password: str) -> User:
