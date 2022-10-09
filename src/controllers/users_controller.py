f0e8f7ac71276160e287ace4ac1d2a25c56ad5f2
ushieru <uzielcocolan@gmail.com>
2022-10-08 19:28:01 -0500

    Fix users_ controller create users

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -1,7 +1,18 @@
+import uuid
+import hashlib
 from typing import List
 from sqlalchemy import select
 from src.database.database import database
 from src.models import User
+
+
+def create_user(name: str, email: str, password: str) -> User:
+    hash_pass = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    user = User(id=str(uuid.uuid4()), name=name,
+                email=email, password=hash_pass)
+    database.add(user)
+    database.commit()
+    return user
 
 
 def get_users(user: User) -> List[User]:
