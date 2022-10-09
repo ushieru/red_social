9271fd11733ce6c4251ca1ac6b1fa206b59bce97
ushieru <uzielcocolan@gmail.com>
2022-10-08 19:29:32 -0500

    Add post routes

---
+++ src/routes/posts.py
@@ -0,0 +1,27 @@
+from operator import itemgetter
+from flask import Blueprint, request
+from src.controllers.posts_controller import create_post, get_posts, get_friends_posts
+from src.decorators.require_bearer_token import require_bearer_token
+
+posts = Blueprint('posts', __name__, url_prefix='/posts')
+
+
+@posts.route('/', methods=['GET'])
+@require_bearer_token
+def index(user):
+    return [post.toJson() for post in get_posts(user)]
+
+
+@posts.route('/', methods=['POST'])
+@require_bearer_token
+def create(user):
+    json = request.get_json()
+    media, description = itemgetter('media', 'description')(json)
+    post = create_post(user, media, description)
+    return post.toJson()
+
+
+@posts.route('/friends', methods=['GET'])
+@require_bearer_token
+def friends_posts(user):
+    return [post.toJson() for post in get_friends_posts(user)]
